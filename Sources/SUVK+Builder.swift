500dc98cad89158e4eadc3b033f5e8dab92c049b
Leem Euna <lonelie@me.com>
2023-02-23 15:28:26 +0900

    Additional fix to bf6cc94.

--- Sources/SUVK+Builder.swift
+++ Sources/SUVK+Builder.swift
@@ -9,27 +9,33 @@ import UIKit
 
 @resultBuilder
 open class ViewGroup {
-    static func buildBlock(_ components: UIView...) -> [UIView] {
+    
+}
+
+extension ViewGroup {
+    public static func buildBlock(_ components: UIView...) -> [UIView] {
         return components
     }
     
-    static func buildEither(first component: UIView) -> [UIView] {
+    public static func buildEither(first component: UIView) -> [UIView] {
         return [component]
     }
     
-    static func buildEither(second component: UIView) -> [UIView] {
+    public static func buildEither(second component: UIView) -> [UIView] {
         return [component]
     }
-    
-    static func buildBlock(_ components: UILabel...) -> [UILabel] {
+}
+
+extension ViewGroup {
+    public static func buildBlock(_ components: UILabel...) -> [UILabel] {
         return components
     }
     
-    static func buildEither(first component: UILabel) -> [UILabel] {
+    public static func buildEither(first component: UILabel) -> [UILabel] {
         return [component]
     }
     
-    static func buildEither(second component: UILabel) -> [UILabel] {
+    public static func buildEither(second component: UILabel) -> [UILabel] {
         return [component]
     }
 }
