4d76a1a3ed11f4442c90912a625a6a48f44125c2
Leem Euna <lonelie@me.com>
2023-02-23 17:50:00 +0900

    Update SampleApp and preview.

--- Resources/SwiftUIViewKitSampleApp/SwiftUIViewKitSampleApp/Main/MainView.swift
+++ Resources/SwiftUIViewKitSampleApp/SwiftUIViewKitSampleApp/Main/MainView.swift
@@ -28,121 +28,146 @@ extension MainView {
     var body: UIView {
         UIVScrollView {
             UIVStackView(spacing: 26.0) {
-                UIImageView(named: "11213734")
-                    .contentMode(.scaleAspectFit)
-                    .onResize(to: self.topIconSize, by: self.disposeBag)
-                    .corner(radius: self.topIconSize.map({ $0.width / 2.0 }), by: self.disposeBag)
-                    .border(width: 3.0)
-                    .border(color: .magenta)
-                    .priority(.required)
-                    .frame(maxWidth: .greatestFiniteMagnitude)
-                UILabel(
-                    UILabel("Awesome!\n")
-                        .font(.systemFont(ofSize: 20.0, weight: .bold))
-                        .lineHeight(26.0)
-                    + UILabel("It's a Label...")
-                        .font(.systemFont(ofSize: 14.0, weight: .thin))
-                        .lineHeight(20.0)
-                )
-                .alignment(.center)
-                .lineLimit(0)
-                .color(.darkGray)
+                self.topView
                 DottedDivider()
                     .setPattern(line: 8.0, space: 2.0)
                 UIVStackView(spacing: 16.0) {
-                    UIHStackView(spacing: 8.0) {
-                        UIView()
-                            .background(.gray)
-                            .frame(width: 4.0)
-                        UILabel(
-                            self.viewModel
-                                .map { $0.userText }
-                                .map { $0.isEmpty ? "Nothing..." : $0 },
-                            by: self.disposeBag
-                        )
-                        .font(.systemFont(ofSize: 14.0, weight: .thin))
-                        .lineLimit(0)
-                        .lineHeight(20.0)
-                        .color(self.viewModel.map({ $0.userText.isEmpty ? .gray : .magenta }), by: disposeBag)
-                    }
-                    UILabel(self.currentText.map({ String(format: "Realtime Text = \"%@\"", $0) }), by: self.disposeBag)
-                        .font(.systemFont(ofSize: 12.0, weight: .thin))
-                        .lineLimit(0)
-                        .lineHeight(14.0)
-                        .lineBreak(.byCharWrapping)
-                        .color(.gray)
-                    UIHStackView(spacing: 8.0) {
-                        UITextField()
-                            .bind(to: self.currentText, by: self.disposeBag)
-                            .configuration {(tf: UITextField) in
-                                tf.placeholder = "Input some text and click button!"
-                            }
-                            .background(
-                                Divider()
-                                    .color(self.isTextFieldFocused.map({ $0 ? .cyan : .lightGray }),
-                                           by: self.disposeBag)
-                                    .frame(maxHeight: .greatestFiniteMagnitude,
-                                           verticalAlignment: .bottom)
-                            )
-                            .onTapGesture(by: self.disposeBag) {gesture in
-                                gesture.view?.becomeFirstResponder()
-                            }
-                            .addTarget(
-                                by: self.disposeBag,
-                                for: .editingDidBegin,
-                                action: {[unowned self] in
-                                    self.isTextFieldFocused.accept(true)
-                                }
-                            )
-                            .addTarget(
-                                by: self.disposeBag,
-                                for: [.editingDidEnd, .editingDidEndOnExit],
-                                action: {[unowned self] in
-                                    self.isTextFieldFocused.accept(false)
-                                }
-                            )
-                            .priority(.defaultHigh)
-                        self.button(text: "CLICK!")
-                            .onTapGesture(by: self.disposeBag) {[unowned self] in
-                                self.endEditing(false)
-                                self.viewModel.unwrappedValue.userText = self.currentText.value
-                            }
-                    }
+                    self.textFieldSampleView
                     Divider(lineWeight: 1.0)
-                    UIHStackView(alignment: .center, spacing: 8.0) {
-                        UILabel("CLICKER")
-                            .font(.systemFont(ofSize: 16.0, weight: .bold))
-                        UIHStackView(alignment: .center, spacing: 8.0) {
-                            self.button(text: "-")
-                                .onTapGesture(by: self.disposeBag) {[unowned self] in
-                                    self.viewModel.unwrappedValue.clickedCount -= 1
-                                }
-                            UILabel(self.viewModel
-                                        .map { String(format: "%lld", $0.clickedCount) }
-                                        .distinctUntilChanged(),
-                                    by: self.disposeBag)
-                                .font(.systemFont(ofSize: 14.0, weight: .thin))
-                                .alignment(.center)
-                                .color(self.viewModel
-                                           .map { $0.clickedCount < 0 ? .red : .green }
-                                           .distinctUntilChanged(),
-                                       by: self.disposeBag)
-                            self.button(text: "+")
-                                .onTapGesture(by: self.disposeBag) {[unowned self] in
-                                    self.viewModel.unwrappedValue.clickedCount += 1
-                                }
-                        }
-                        .priority(.required)
-                    }
+                    self.clickerSampleView
                     Divider(lineWeight: 1.0)
-                    self.button(text: "Move to SecondView!")
-                        .onTapGesture(by: self.disposeBag, publish: self.didTappedNavigateToSecondView)
+                    self.navigateToSecondViewButton
                 }
             }
             .padding(.top, 20.0)
             .padding(.horizontal, 16.0)
             .padding(.bottom, 12.0)
         }
+    }
+}
+
+//MARK: - Subviews
+extension MainView {
+    private var topView: UIView {
+        UIVStackView(spacing: 24.0) {
+            UIImageView(named: "11213734")
+                .contentMode(.scaleAspectFit)
+                .onResize(to: self.topIconSize, by: self.disposeBag)
+                .corner(radius: self.topIconSize.map({ $0.width / 2.0 }), by: self.disposeBag)
+                .border(width: 3.0)
+                .border(color: .magenta)
+                .priority(.required)
+                .frame(maxWidth: .greatestFiniteMagnitude)
+            UILabel(
+                UILabel("Awesome!\n")
+                    .font(.systemFont(ofSize: 20.0, weight: .bold))
+                    .lineHeight(26.0)
+                + UILabel("It's a Label...")
+                    .font(.systemFont(ofSize: 14.0, weight: .thin))
+                    .lineHeight(20.0)
+            )
+            .alignment(.center)
+            .lineLimit(0)
+            .color(.darkGray)
+        }
+    }
+    
+    private var textFieldSampleView: UIView {
+        UIVStackView(spacing: 12.0) {
+            UIHStackView(spacing: 8.0) {
+                UIView()
+                    .background(.gray)
+                    .frame(width: 4.0)
+                UILabel(
+                    self.viewModel
+                        .map { $0.userText }
+                        .map { $0.isEmpty ? "Nothing..." : $0 },
+                    by: self.disposeBag
+                )
+                .font(.systemFont(ofSize: 14.0, weight: .thin))
+                .lineLimit(0)
+                .lineHeight(20.0)
+                .color(self.viewModel.map({ $0.userText.isEmpty ? .gray : .magenta }), by: disposeBag)
+            }
+            UILabel(self.currentText.map({ String(format: "Realtime Text = \"%@\"", $0) }), by: self.disposeBag)
+                .font(.systemFont(ofSize: 12.0, weight: .thin))
+                .lineLimit(0)
+                .lineHeight(14.0)
+                .lineBreak(.byCharWrapping)
+                .color(.gray)
+            UIHStackView(spacing: 8.0) {
+                UITextField()
+                    .bind(to: self.currentText, by: self.disposeBag)
+                    .configuration {(tf: UITextField) in
+                        tf.placeholder = "Input some text and click button!"
+                    }
+                    .background(
+                        Divider()
+                            .color(self.isTextFieldFocused.map({ $0 ? .cyan : .lightGray }),
+                                   by: self.disposeBag)
+                            .frame(maxHeight: .greatestFiniteMagnitude,
+                                   verticalAlignment: .bottom)
+                    )
+                    .onTapGesture(by: self.disposeBag) {gesture in
+                        gesture.view?.becomeFirstResponder()
+                    }
+                    .addTarget(
+                        by: self.disposeBag,
+                        for: .editingDidBegin,
+                        action: {[unowned self] in
+                            self.isTextFieldFocused.accept(true)
+                        }
+                    )
+                    .addTarget(
+                        by: self.disposeBag,
+                        for: [.editingDidEnd, .editingDidEndOnExit],
+                        action: {[unowned self] in
+                            self.isTextFieldFocused.accept(false)
+                        }
+                    )
+                    .priority(.defaultHigh)
+                self.button(text: "CLICK!")
+                    .onTapGesture(by: self.disposeBag) {[unowned self] in
+                        self.endEditing(false)
+                        self.viewModel.unwrappedValue.userText = self.currentText.value
+                    }
+            }
+        }
+    }
+    
+    private var clickerSampleView: UIView {
+        UIVStackView(spacing: 12.0) {
+            UIHStackView(alignment: .center, spacing: 8.0) {
+                UILabel("CLICKER")
+                    .font(.systemFont(ofSize: 16.0, weight: .bold))
+                UIHStackView(alignment: .center, spacing: 8.0) {
+                    self.button(text: "-")
+                        .onTapGesture(by: self.disposeBag) {[unowned self] in
+                            self.viewModel.unwrappedValue.clickedCount -= 1
+                        }
+                    UILabel(self.viewModel
+                                .map { String(format: "%lld", $0.clickedCount) }
+                                .distinctUntilChanged(),
+                            by: self.disposeBag)
+                        .font(.systemFont(ofSize: 14.0, weight: .thin))
+                        .alignment(.center)
+                        .color(self.viewModel
+                                   .map { $0.clickedCount < 0 ? .red : .green }
+                                   .distinctUntilChanged(),
+                               by: self.disposeBag)
+                    self.button(text: "+")
+                        .onTapGesture(by: self.disposeBag) {[unowned self] in
+                            self.viewModel.unwrappedValue.clickedCount += 1
+                        }
+                }
+                .priority(.required)
+            }
+        }
+    }
+    
+    private var navigateToSecondViewButton: UIView {
+        self.button(text: "Move to SecondView!")
+            .onTapGesture(by: self.disposeBag, publish: self.didTappedNavigateToSecondView)
     }
     
     private func button(text: String) -> UIView {
